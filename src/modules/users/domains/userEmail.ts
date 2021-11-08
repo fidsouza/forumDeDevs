aa5b363b545db5613d789a4655335b312d8498dc
Filipe <filipe.cdesouza@gmail.com>
2021-11-07 23:14:37 -0300

    feat: implement email validator and format email

--- src/modules/users/domains/userEmail.ts
+++ src/modules/users/domains/userEmail.ts
@@ -14,7 +14,21 @@ export class UserEmail extends ValueObject<UserEmailProps> {
     return this.props.value;
   }
 
+  private static isValidEmail(email: string) {
+    const re =
+      /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(email);
+  }
+
+  private static format(email: string): string {
+    return email.trim().toLowerCase();
+  }
+
   public static create(email: string): Result<UserEmail> {
-    return Result.ok<UserEmail>(new UserEmail({ value: email }));
+    if (!this.isValidEmail(email)) {
+      return Result.fail<UserEmail>("Email address is invalid");
+    }
+    return Result.ok<UserEmail>(new UserEmail({ value: this.format(email) }));
   }
 }
