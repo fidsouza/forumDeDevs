914d5398c4bc83eebf00fb80b297d5f0f217dd20
Filipe <filipe.cdesouza@gmail.com>
2021-11-07 23:26:38 -0300

    test: implement email validator when is false

--- src/modules/users/domains/userEmail.spec.ts
+++ src/modules/users/domains/userEmail.spec.ts
@@ -6,9 +6,14 @@ let email: UserEmail;
 
 describe("UserEmail validation", () => {
   test("should be able to create a valid email", () => {
-    emailOrError = UserEmail.create("filipe_costa@hotmail.com");
+    emailOrError = UserEmail.create("filipe.cdesouza@gmail.com");
     expect(emailOrError.isSuccess).toBe(true);
     email = emailOrError.getValue();
-    expect(email.props.value).toBe("filipe_costa@hotmail.com");
+    expect(email.props.value).toBe("filipe.cdesouza@gmail.com");
+  });
+
+  test("Should fail to create an invalid email", () => {
+    emailOrError = UserEmail.create("any_email");
+    expect(emailOrError.isSuccess).toBe(false);
   });
 });
